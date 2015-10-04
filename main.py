0f31495784ef80099558e36e27741dcf24f7131f
exasdf <dmytrodondyk@gmail.com>
2015-10-04 01:05:17 -0400

    Test

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
 
 
+class Animal:
+   def __init__(self):
+      print "Animal created"
 
-if __name__ == '__main__':
-    print ("Hello world!!")
-    pass
+   def whoAmI(self):
+      print "Animal"
+
+   def eat(self):
+      print "Eating"
+
+
+class Dog(Animal):
+   def __init__(self):
+      Animal.__init__(self)
+      print "Dog created"
+
+   def whoAmI(self):
+      print "Dog"
+
+   def bark(self):
+      print "Woof!"
+
+
+d = Dog()
+d.whoAmI()
+d.eat()
+d.bark()
