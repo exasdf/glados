c98dc29a6466f14a2a0c10a8946d215662a4ca44
exasdf <dmytrodondyk@gmail.com>
2015-10-04 23:11:35 -0400

    Test

--- main.py
+++ main.py
@@ -1,29 +1,8 @@
+import click
+import module
 
 
-class Animal:
-   def __init__(self):
-      print "Animal created"
-
-   def whoAmI(self):
-      print "Animal"
-
-   def eat(self):
-      print "Eating"
-
-
-class Dog(Animal):
-   def __init__(self):
-      Animal.__init__(self)
-      print "Dog created"
-
-   def whoAmI(self):
-      print "Dog"
-
-   def bark(self):
-      print "Woof!"
-
-
-dog = Dog()
+dog = module.Dog()
 dog.whoAmI()
 dog.eat()
 dog.bark()
--- module.py
+++ module.py
@@ -1,1 +1,20 @@
-__author__ = 'ex'
+class Animal:
+   def __init__(self):
+      print "Animal created"
+   def whoAmI(self):
+      print "Animal"
+
+   def eat(self):
+      print "Eating"
+
+
+class Dog(Animal):
+   def __init__(self):
+      Animal.__init__(self)
+      print "Dog created"
+
+   def whoAmI(self):
+      print "Dog"
+
+   def bark(self):
+      print "Woof!"
