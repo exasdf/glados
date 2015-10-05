b2184a9e82a9603baf645868e8b6ec30e4734db0
exasdf <dmytrodondyk@gmail.com>
2015-10-04 23:58:17 -0400

    Test

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-import click
 import module
-
+import argparse
 
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("echo", help="echo the string you use here")
+    args = parser.parse_args()
+    print args.echo
+
 
     dog = module.Dog()
     dog.whoAmI()
