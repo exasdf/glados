3db7e12980e18afd43bd185c5713b5830b8dc3b0
exasdf <dmytrodondyk@gmail.com>
2015-10-04 23:20:37 -0400

    Test

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ import click
 import module
 
 
-dog = module.Dog()
-dog.whoAmI()
-dog.eat()
-dog.bark()
+
+
+if __name__ == '__main__':
+
+    dog = module.Dog()
+    dog.whoAmI()
+    dog.eat()
+    dog.bark()
