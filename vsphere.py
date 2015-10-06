d526dec32306d86ab5c48533835da8dec20983cc
exasdf <dmytrodondyk@gmail.com>
2015-10-06 00:46:12 -0400

    Test

--- vsphere.py
+++ vsphere.py
@@ -10,12 +10,13 @@ class vsphere(object):
 
 def main():
     parser = argparse.ArgumentParser(description='Process some integers.')
-
-    parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer for the accumulator')
+    #https://docs.python.org/3/library/argparse.html
+    parser.add_argument('--integers', metavar='N', type=int, nargs='+', help='an integer for the accumulator')
     parser.add_argument('--sum', dest='accumulate', action='store_const', const=sum, default=max, help='sum the integers')
 
     args = parser.parse_args()
     print(args.accumulate(args.integers))
+    print args.integers[1]
 
 
 
