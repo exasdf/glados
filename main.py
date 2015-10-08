aa79cb5d0e78754f19c5e4e57e2615032544514b
exasdf <dmytrodondyk@gmail.com>
2015-10-08 01:17:25 -0400

    Dmytro Dondyk

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import argparse
 import engine.importdir
+engine.importdir.do("module", globals())
 
 
-def main():
+def user_Arguments():
     parser = argparse.ArgumentParser(description='Process some integers.')
-
     parser.add_argument('-on', default="Empty", metavar='Example', nargs='+', help='Help')
     parser.add_argument('-do', default="Empty", metavar='Example', nargs='+', help='Help')
     parser.add_argument('-run', default="Empty", metavar='Example', nargs='+', help='Help')
-
     parser.add_argument('-m', '-module', default="Empty", metavar='Example', nargs='+', help='Help')
-
 
     args = parser.parse_args()
     print args.run
+
+
+def main():
+    user_Arguments()
+
 
 
 
