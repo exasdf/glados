dbf3b5607eaa2958ac25376c69a284a1d58a15db
exasdf <dmytrodondyk@gmail.com>
2015-10-08 01:59:26 -0400

    Dmytro Dondyk

--- main.py
+++ main.py
@@ -3,22 +3,61 @@ import engine.importdir
 engine.importdir.do("module", globals())
 
 
-def user_Arguments():
-    parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('-on', default="Empty", metavar='Example', nargs='+', help='Help')
-    parser.add_argument('-do', default="Empty", metavar='Example', nargs='+', help='Help')
-    parser.add_argument('-run', default="Empty", metavar='Example', nargs='+', help='Help')
-    parser.add_argument('-m', '-module', default="Empty", metavar='Example', nargs='+', help='Help')
 
-    args = parser.parse_args()
-    print args.run
+def get_User_Arguments():
+
+    getArguments = argparse.ArgumentParser(description='Process some integers.')
+    getArguments.add_argument('-on',
+                             default="Empty",
+                             metavar='Example',
+                             nargs='+',
+                             help='Help')
+
+    getArguments.add_argument('-do',
+                             default="Empty",
+                             metavar='Example',
+                             nargs='+',
+                             help='Help')
+
+    getArguments.add_argument('-run',
+                             default="Empty",
+                             metavar='Example',
+                             nargs='+',
+                             help='Help')
+
+    getArguments.add_argument('-m',
+                             '-module',
+                             default="Empty",
+                             metavar='Example',
+                             nargs='+',
+                             help='Help')
+
+    arguments = getArguments.parse_args()
+
+    return arguments
+
+def execute(do, on, run, ):
+
+    #Parses module to do engine to return module instructions.
+    if do != None and on != None and run == None:
+        COMMAND = do.call(do)  # @UndefinedVariable
+        #Executes command over ssh
+        engine.ssh.run_Module( on, "vagrant", "vagrant", COMMAND )  # @UndefinedVariable
+
+    #Else run the command straight on hosts
+    elif do == None and on != None and run != None:
+        COMMAND = run
+        #Executes command over ssh
+        engine.ssh.run_Command( on, "vagrant", "vagrant", COMMAND )  # @UndefinedVariable
+
+    else:
+        print '\033[91m' + u'\u2716'  + "  Invalid operation" + '\033[0m'
+        exit()
 
 
 def main():
-    user_Arguments()
-
-
-
+    user = get_User_Arguments()
+    print user.run
 
 if __name__ == '__main__':
     main()
